02f5668cf680b39983ed3fc2ecde5a0cc115fd72
vboxuser <vboxuser@ubuntu-server-01.myguest.virtualbox.org>
2023-07-19 17:23:29 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { screenType } from "./store/common";
+	import List from "./pages/List.svelte";
+	import Detail from "./pages/Detail.svelte";
+	import "./assets/css/reset.css";
+	import "./assets/css/common.css";
+</script>
+
+<style lang="scss">
+	main {
+		width: 544px;
+		padding: 20px;
+	}
+</style>
+
+<main>
+	<h1>メモアプリ</h1>
+	{#if $screenType === 0}
+		<List />
+	{:else if $screenType === 1}
+		<Detail />
+	{/if}
+</main>
